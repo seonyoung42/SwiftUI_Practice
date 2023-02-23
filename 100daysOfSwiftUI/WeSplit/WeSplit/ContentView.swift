fa13d429c4c7dae45296856f7d22e56e29af00b3
seonyoung <4tjsdud2@naver.com>
2023-02-23 18:52:27 +0900

    [feat] NavigationView

--- 100daysOfSwiftUI/WeSplit/WeSplit/ContentView.swift
+++ 100daysOfSwiftUI/WeSplit/WeSplit/ContentView.swift
@@ -10,24 +10,28 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        Form {
-            Group {
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
+        NavigationView {
+            Form {
+                Group {
+                    Text("Hello, World!")
+                    Text("Hello, World!")
+                    Text("Hello, World!")
+                }
+                
+                Group {
+                     Text("Hello, World!")
+                     Text("Hello, World!")
+                     Text("Hello, World!")
+                 }
+                
+                Section {
+                    Text("Hello, World!")
+                    Text("Hello, World!")
+                    Text("Hello, World!")
+                }
             }
-            
-            Group {
-                 Text("Hello, World!")
-                 Text("Hello, World!")
-                 Text("Hello, World!")
-             }
-            
-            Section {
-                Text("Hello, World!")
-                Text("Hello, World!")
-                Text("Hello, World!")
-            }
+            .navigationTitle("Swift UI")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
