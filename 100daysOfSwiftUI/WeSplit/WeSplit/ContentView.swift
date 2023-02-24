a25a2d418edff8fe32d261925e1dfb4b1ab36c7a
seonyoung <4tjsdud2@naver.com>
2023-02-24 17:02:18 +0900

    [feat] SegmentControl, Section header

--- 100daysOfSwiftUI/WeSplit/WeSplit/ContentView.swift
+++ 100daysOfSwiftUI/WeSplit/WeSplit/ContentView.swift
@@ -30,6 +30,17 @@ struct ContentView: View {
                 }
                 
                 Section {
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section {
                     Text(checkAmount,
                          format: .currency(code: Locale.current.currencyCode ?? "USD"))
                 }
