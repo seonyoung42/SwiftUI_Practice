d4f13d884e8263fc3447fa13d50ef59e7c7fe2c5
seonyoung <4tjsdud2@naver.com>
2023-02-24 17:10:00 +0900

    [feat] computed property 'totalPerPerson'

--- 100daysOfSwiftUI/WeSplit/WeSplit/ContentView.swift
+++ 100daysOfSwiftUI/WeSplit/WeSplit/ContentView.swift
@@ -14,6 +14,17 @@ struct ContentView: View {
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
         NavigationView {
             Form {
@@ -41,7 +52,7 @@ struct ContentView: View {
                 }
                 
                 Section {
-                    Text(checkAmount,
+                    Text(totalPerPerson,
                          format: .currency(code: Locale.current.currencyCode ?? "USD"))
                 }
             }
