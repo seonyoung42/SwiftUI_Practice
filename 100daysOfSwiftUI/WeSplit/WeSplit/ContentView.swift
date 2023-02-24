8184ab6f31a379048f2f3408b800267a36cf8ddd
seonyoung <4tjsdud2@naver.com>
2023-02-24 17:16:37 +0900

    [feat] hideKeyboard

--- 100daysOfSwiftUI/WeSplit/WeSplit/ContentView.swift
+++ 100daysOfSwiftUI/WeSplit/WeSplit/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
@@ -33,6 +34,8 @@ struct ContentView: View {
                               value: $checkAmount,
                               format: .currency(code: Locale.current.currencyCode ?? "USD"))
                     .keyboardType(.decimalPad)
+                    .focused($amountIsFocused)
+                    
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2..<100) {
                             Text("\($0) people")
@@ -57,6 +60,14 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
