3f1c52b8dae4a747bf00b5c869a8a1dd771efe4c
seonyoung <4tjsdud2@naver.com>
2023-02-28 15:33:58 +0900

    [feat] Guess the flag

--- 100daysOfSwiftUI/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ 100daysOfSwiftUI/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,18 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State private var countries = ["estonia", "france", "germany", "ireland",
+                                    "italy", "monaco", "nigeria", "poland",
+                                    "russia", "spain", "uk", "us"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        Button("Show Alert") {
-            showingAlert = true
-        }/// isPresented가 alert가 dismiss될 때 showingAlert를 false로 만들어줌 -> two-way binding ('$')
-        .alert("Important Message", isPresented: $showingAlert)  {
-            Button("Delete", role: .destructive) { }
-            Button("Cancel", role: .cancel) { }
-        } message: {
-            Text("Please read this")
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.2),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.2)
+            ], center: .top, startRadius: 200, endRadius: 700)
+            .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Text("Guess The Flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundColor(.white)
+                
+                VStack(spacing: 30) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .shadow(radius: 5.0)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 30)
+                .background(.thinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: ??? ")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                
+                Spacer()
+            }
+            .padding()
         }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your scroe is ???")
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        scoreTitle = number == correctAnswer ? "Correct" : "Wrong"
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
