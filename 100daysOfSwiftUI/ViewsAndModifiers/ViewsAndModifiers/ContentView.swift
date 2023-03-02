ef06cb4afdf96ad9e4149162ff44f61d6ab46c88
seonyoung <4tjsdud2@naver.com>
2023-03-02 21:42:18 +0900

    [feat] some View

--- 100daysOfSwiftUI/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ 100daysOfSwiftUI/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -8,17 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    /// 어떤 View가 있을 지 모르기 때문에 some 붙임
+    /// 정확한 타입인 ModifiedContent<ModifiedContent< ... 를 작성해도 되지만 이것은 너무 비효율적임
     var body: some View {
         Button("Hello, world!") {
-            
+            print(type(of: self.body))
+            ///ModifiedContent<ModifiedContent<Button<Text>, _FrameLayout>, _BackgroundStyleModifier<Color>>
         }
-        .tint(.black)
         .frame(width: 100, height: 100)
         .background(.red)
-        .padding()
-        .background(.blue)
-        .padding()
-        .background(.yellow)
     }
 }
 
