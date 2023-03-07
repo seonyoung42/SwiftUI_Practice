a7bef0d3ec1a080fd54a5a73e37140e25ba3ace4
seonyoung <4tjsdud2@naver.com>
2023-03-07 20:32:25 +0900

    [feat] Date handling

--- 100daysOfSwiftUI/BetterRest/BetterRest/ContentView.swift
+++ 100daysOfSwiftUI/BetterRest/BetterRest/ContentView.swift
@@ -11,11 +11,29 @@ struct ContentView: View {
     @State private var wakeUp = Date.now
     
     var body: some View {
-       DatePicker("Please enter a date",
-                  selection: $wakeUp,
-                  in: Date.now...,
-                  displayedComponents: .date)
-        
+        VStack {
+            Text(Date.now, format: .dateTime.day().month().year())
+            Text(Date.now.formatted(date: .long, time: .omitted))
+        }
+    }
+    
+    func example() {
+        let now = Date.now
+        let tomorrow = Date.now.addingTimeInterval(86400)
+        let range = now...tomorrow
+    }
+    
+    func example2() {
+        var component = DateComponents()
+        component.hour = 8
+        component.minute = 0
+        let date = Calendar.current.date(from: component) ?? Date.now
+    }
+    
+    func example3() {
+        let components = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
+        let hour = components.hour ?? 0
+        let miniute = components.minute ?? 0
     }
 }
 
