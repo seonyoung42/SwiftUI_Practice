3650c5281cfcb2b39a8d18b0d697799d779cbc40
seonyoung <4tjsdud2@naver.com>
2023-03-08 15:10:17 +0900

    [feat] View Layout

--- 100daysOfSwiftUI/BetterRest/BetterRest/ContentView.swift
+++ 100daysOfSwiftUI/BetterRest/BetterRest/ContentView.swift
@@ -9,31 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
     
     var body: some View {
-        VStack {
-            Text(Date.now, format: .dateTime.day().month().year())
-            Text(Date.now.formatted(date: .long, time: .omitted))
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours",
+                        value: $sleepAmount,
+                        in: 4...12,
+                        step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups",
+                        value: $coffeeAmount,
+                        in: 1...20)
+            }
+            .padding()
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBadTime)
+            }
         }
     }
     
-    func example() {
-        let now = Date.now
-        let tomorrow = Date.now.addingTimeInterval(86400)
-        let range = now...tomorrow
-    }
-    
-    func example2() {
-        var component = DateComponents()
-        component.hour = 8
-        component.minute = 0
-        let date = Calendar.current.date(from: component) ?? Date.now
-    }
-    
-    func example3() {
-        let components = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
-        let hour = components.hour ?? 0
-        let miniute = components.minute ?? 0
+    func calculateBadTime() {
+        print("calculateBadTime")
     }
 }
 
