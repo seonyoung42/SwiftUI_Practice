e33306c6056f32e8e6243c611e6dca877ef386b1
seonyoung <4tjsdud2@naver.com>
2023-03-09 18:03:54 +0900

    [feat] List

--- 100daysOfSwiftUI/WordScramble/WordScramble/ContentView.swift
+++ 100daysOfSwiftUI/WordScramble/WordScramble/ContentView.swift
@@ -8,9 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    let people = ["Finn", "Leia", "Luke", "Rey"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List {
+            Section("Section 1") {
+                Text("Static row 1")
+                Text("Static row 2")
+            }
+            
+            ForEach(people, id:\.self) {
+                Text($0)
+            }
+            
+            Text("Static row")
+        }
     }
 }
 
