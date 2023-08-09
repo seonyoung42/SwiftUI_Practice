8b5812f205dbe4395ac31af91bdadf7b4e0c2d49
seonyoung <4tjsdud2@naver.com>
2023-08-09 17:35:08 +0900

    [chore] initial commit

---
+++ Startbucks/Startbucks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Startbucks
+//
+//  Created by 장선영 on 2023/08/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Startbucks/Startbucks/StartbucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartbucksApp.swift
+//  Startbucks
+//
+//  Created by 장선영 on 2023/08/09.
+//
+
+import SwiftUI
+
+@main
+struct StartbucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
