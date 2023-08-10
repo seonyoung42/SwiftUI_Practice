c1c01ced7f0676a143f98d36ec23b9d3bf9df82d
seonyoung <4tjsdud2@naver.com>
2023-08-10 13:45:07 +0900

    [Chore] initial commit

---
+++ FindCovid19/FindCovid19/FindCovid19App.swift
@@ -0,0 +1,17 @@
+//
+//  FindCovid19App.swift
+//  FindCovid19
+//
+//  Created by 장선영 on 2023/08/10.
+//
+
+import SwiftUI
+
+@main
+struct FindCovid19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
